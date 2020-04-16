43edbc828b4f9a225f3cbd33a034e57c0cfd6d2e
Juhana Kuparinen <jmkupa@utu.fi>
2020-04-16 13:38:23 +0300

    Ruudukko tehty peliruutuun.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Kärmespeli
+
+By: Johanna Seulu, Juhana Kuparinen, Juho Ollila
+
+## Pelistä
+
+Kärmespeli on projekti, joka on toteutettu Soveltava projekti 2020 kurssia varten.
+Se on yksinkertainen matopeli, joka on luotu pythonin pygame -pelikirjastolla.
+
+### Huom. Juho ja Johanna!
+
+Peliä on luultavasti helpointa kehittää PyCharm IDE:llä. Saatte ladattua
+"professional" version kehitysympäristöstä yliopisto tunnuksilla.
+
+Kun olette ladanneet PyCharmin ja esim. pygamen importtaus ei toimi,
+menkää projektissa _settings_ -> _Project: Karmespeli_ -> _Python Interpreter_
+ ja painakaa + ikonia oikeassa kulmassa. Kirjoittakaa hakuun pygame ja lisätkää paketti.
+ 
+ On myös mahdollista, että teidän pitää ladata pygame koneellenne manuaalisesti.
+ Tämä hoituu komentoriviltä __py -m pip install pygame__ komennolla.
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Soveltava projekti 2020
 # Johanna Seulu, Juhana Kuparinen, Juho Ollila
 
-VERSION = 0.1
+VERSION = 0.2
 
 import pygame, sys
 from pygame.locals import *
@@ -10,8 +10,8 @@ from pygame.locals import *
 
 # Snake luokka joka perii Sprite luokan
 class Snake(pygame.sprite.Sprite):
-    x = 400
-    y = 400
+    x = 0
+    y = 200
     pos = (x, y)
     direction = "left"
 
@@ -47,7 +47,7 @@ class Snake(pygame.sprite.Sprite):
         self.direction = "left"
 
 class Game:
-    windowWidth = 1000
+    windowWidth = 800
     windowHeight = 800
     screenResolution = (windowWidth, windowHeight)
     clock = pygame.time.Clock()
@@ -63,9 +63,12 @@ class Game:
 
     def game_loop(self):
         # Peli looppi
+        self.display_screen.fill((0, 0, 0))
         while self.running:
             # Varmistetaan että peli ei mene yli 60 fps:n
             self.clock.tick(60)
+            self.drawGrid()
+            self.snake.update()
             # Tapahtuma looppi
             for event in pygame.event.get():
                 # Kaksi ensimmäistä if lausetta käsittelevät pelistä poistumisen
@@ -88,27 +91,35 @@ class Game:
                     if event.key == K_DOWN:
                         self.snake.moveDown()
 
-            self.snake.update()
-            self.display_screen.blit(self.background, (0, 0))
-            self.display_screen.blit(self.snake.image, self.snake.pos)
+            # blit() metodilla piirretään näytölle
+            #self.display_screen.blit(self.background, (0, 0))
+            #self.display_screen.blit(self.snake.image, self.snake.pos)
             # Metodia flip kutsutaan jotta näyttö päivittyy
-            pygame.display.flip()
+            # pygame.display.flip()
+            pygame.display.update()
 
     # Funktio jolla aloitetaan peli
     def start_game(self):
+        # pygame.init() -metodia täytyy kutsua, jotta pelimootroi
+        # käynnistyy.
         pygame.init()
+
+        # Määritellään näytön ominaisuuksia kuten resoluutio...
         self.display_screen = pygame.display.set_mode(self.screenResolution)
         icon = pygame.image.load('icon.png')
         pygame.display.set_caption("Kärmespeli")
         pygame.display.set_icon(icon)
 
-        # Vihreä tausta
-        self.background = pygame.Surface(self.display_screen.get_size())
-        self.background = self.background.convert()
-        self.background.fill((0, 200, 0))
-
         # Käynnistetään itse peli...
         self.game_loop()
+
+    # drawGrid() -metodi piirtää Kärmespeliin ruudukon
+    def drawGrid(self):
+        gridSize = 20
+        for i in range(self.windowWidth):
+            for j in range(self.windowHeight):
+                rect = pygame.Rect(i*gridSize, j*gridSize, gridSize, gridSize)
+                pygame.draw.rect(self.display_screen, (255, 255, 255), rect, 1)
 
 
 if __name__ == "__main__":
