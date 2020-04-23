c8670a55f3d5e6187db1a45c0eba86ed5e902a20
Jugebox <jugebox99@gmail.com>
2020-04-23 13:22:24 +0300

    Peli loppui ikkunaan lisätty Pelaa uudelleen ja Poistu pelistä näppäimet Menu luokkaan lisätty myös drawtext funktio

--- game_modules/game_engine.py
+++ game_modules/game_engine.py
@@ -8,6 +8,9 @@ from game_modules.Snake import Snake
 from game_modules.Apple import Apple
 from game_modules.Obstacle import Obstacle
 
+#################################
+# MENU LUOKKA
+#################################
 
 class Menu:
     # Kun peli käynnistetään, tällä enum -luokalla tiedetään
@@ -29,6 +32,7 @@ class Menu:
     clock = pygame.time.Clock()
     menu_screen = pygame.display.set_mode(menuResolution)
 
+    #Määritellään valmiiksi värejä RGB arvojen avulla
     black = (10, 10, 10)
     white = (255, 255, 255)
     green = (150, 185, 150)
@@ -43,21 +47,28 @@ class Menu:
     def __init__(self):
         self.running = True
 
+    def drawText(self, text, font, textColor, location):
+        textSurface, textRectangle = self.text_object(text, font, textColor)
+        textRectangle.center = location
+        self.menu_screen.blit(textSurface, textRectangle)
+
     def text_object(self, text, font, colour):
         textSurface = font.render(text, True, colour)
         return textSurface, textSurface.get_rect()
 
+
     # ic = inactive colour eli väri joka on käytössä kun hiiri ei ole napin päällä
     # ac = active colour eli väri joka tulee käyttöön kun hiirellä mennään napin päälle
-    def button(self, message, x, y, width, height, ic, ac, action=None):
+    def button(self, message, x, y, width, height, ic, ac, action=None, fontsize=None):
 
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
         # Näppäimen luominen
+        #Kun hiiri on näppäimen päällä
         if x + width > mouse[0] > x and y + height > mouse[1] > y:
             pygame.draw.rect(self.menu_screen, ac, (x, y, width, height))
-
+            #Tapahtuma kun hiirellä klikkaa
             if click[0] == 1 and action != None:
                 # Päävalikon toiminnot
                 if action == "playsp":
@@ -91,15 +102,17 @@ class Menu:
                     Peli = Game(self.Difficulties.Hard, self.Gamemodes.Duel)
                     Peli.start_game(800, 600)
 
-
+        #Kun hiiri ei ole näppäimen päällä
         else:
             pygame.draw.rect(self.menu_screen, ic, (x, y, width, height))
 
         # Teksti
-        textCont = pygame.font.Font('OpenSans-Regular.ttf', 40)
-        textSurf, textRect = self.text_object(message, textCont, self.black)
-        textRect.center = (math.floor((self.menuWidth / 2)), y + 35)
-        self.menu_screen.blit(textSurf, textRect)
+        if fontsize != None:
+            font = pygame.font.Font('OpenSans-Regular.ttf', fontsize)
+            self.drawText(message, font, self.black, ((x + width / 2), y + 35))
+        else:
+            font = pygame.font.Font('OpenSans-Regular.ttf', 40)
+            self.drawText(message, font, self.black, ((x + width/2), y + 35))
 
     # Aloitus valikko pelille
     def main_menu(self):
@@ -110,6 +123,9 @@ class Menu:
         self.menu_screen = pygame.display.set_mode(self.menuResolution)
 
         pygame.init()
+        icon = pygame.image.load('icon.png')
+        pygame.display.set_caption("Kärmespeli")
+        pygame.display.set_icon(icon)
         menu = True
 
         # Valikko looppi
@@ -121,10 +137,9 @@ class Menu:
 
             # Main menu otsikko ja tausta
             self.menu_screen.fill(self.black)
-            textCont = pygame.font.Font('OpenSans-Bold.ttf', 100)
-            textSurf, textRect = self.text_object("KÄRMESPELI", textCont, self.white)
-            textRect.center = (math.floor((self.menuWidth / 2)), 100)
-            self.menu_screen.blit(textSurf, textRect)
+            text = "KÄRMESPELI"
+            font = pygame.font.Font('OpenSans-Bold.ttf', 100)
+            self.drawText(text, font, self.white,(math.floor((self.menuWidth / 2)), 100))
 
             # Nappien luonti
             self.button("YKSINPELI", 160, 200, 500, 75, self.green, self.light_green, "playsp")
@@ -146,20 +161,18 @@ class Menu:
                     quit()
 
             self.menu_screen.fill((10, 10, 10))
-            textCont = pygame.font.Font('OpenSans-Regular.ttf', 50)
-            textSurf, textRect = self.text_object("Juhana Kuparinen", textCont, self.white)
-            textRect.center = (400, 200)
-            self.menu_screen.blit(textSurf, textRect)
 
-            textCont = pygame.font.Font('OpenSans-Regular.ttf', 50)
-            textSurf, textRect = self.text_object("Juho Ollila", textCont, self.white)
-            textRect.center = (400, 300)
-            self.menu_screen.blit(textSurf, textRect)
+            text = "Juhana Kuparinen"
+            font = pygame.font.Font('OpenSans-Regural.ttf', 50)
+            self.drawText(text, font, self.white, (400, 200))
 
-            textCont = pygame.font.Font('OpenSans-Regular.ttf', 50)
-            textSurf, textRect = self.text_object("Johanna Seulu", textCont, self.white)
-            textRect.center = (400, 400)
-            self.menu_screen.blit(textSurf, textRect)
+            text = "Juho Ollila"
+            font = pygame.font.Font('OpenSans-Regural.ttf', 50)
+            self.drawText(text, font, self.white, (400, 300))
+
+            text = "Johanna Seulu"
+            font = pygame.font.Font('OpenSans-Regural.ttf', 50)
+            self.drawText(text, font, self.white, (400, 400))
 
             pygame.display.update()
             pygame.time.wait(5000)
@@ -167,8 +180,8 @@ class Menu:
 
     # Tason valinta valikko
     def sp_select(self):
-        pygame.quit()
         pygame.init()
+
         self.menuWidth = 800
         self.menuHeight = 600
         self.menuResolution = (self.menuWidth, self.menuHeight)
@@ -187,8 +200,8 @@ class Menu:
             self.clock.tick(15)
 
     def duel_select(self):
-        pygame.quit()
         pygame.init()
+
         self.menuWidth = 800
         self.menuHeight = 600
         self.menuResolution = (self.menuWidth, self.menuHeight)
@@ -344,7 +357,6 @@ class Game:
                     if self.snake.collideWithOther(self.other_snake.snakeLocation()) or self.other_snake.collideWithOther(self.snake.snakeLocation()):
                         if not self.game_over:
                             self.game_over = not self.game_over
-                        self
 
                 if self.snake.snakeLocation() == self.apple.appleLocation():
                     self.apple.newApple(self.gridSize)
@@ -364,13 +376,15 @@ class Game:
                     if self.obstacles[i].obstacleLocation() == self.snake.snakeLocation()\
                             or self.obstacles[i].obstacleLocation() == self.other_snake.snakeLocation():
                         self.game_over = not self.game_over
-
+            #Pause ruutu
             if self.pause:
                 self.display_screen.fill((10, 10, 10))
                 font = pygame.font.Font('OpenSans-Regular.ttf', 60)
                 self.drawText("Pysäytetty...", font, (255, 255, 255), (math.floor((self.windowWidth / 2)), self.windowHeight / 2))
 
+            #Pelin lopetusruutu
             if self.game_over:
+                menu = Menu()
                 restart = ""
                 text = "PELI PÄÄTTYI"
                 font = pygame.font.Font('OpenSans-Regular.ttf', 60)
@@ -384,20 +398,70 @@ class Game:
                 else:
                     pygame.draw.rect(self.display_screen, (10, 10, 10), (math.floor(self.windowWidth / 2.6), 25, 150, 50))
                     # Pelaaja 1
-                    text = "Pelaaja 1: " + str(self.score1)
+                    text = "Pelaaja 1 pisteet: " + str(self.score1)
                     font = pygame.font.Font('OpenSans-Regular.ttf', 25)
                     self.drawText(text, font, (255, 255, 255), (math.floor((self.windowWidth / 2)), math.floor((self.windowHeight / 2))))
 
                     # Pelaaaja 2
-                    text = "Pelaaja 2: " + str(self.score2)
+                    text = "Pelaaja 2 pisteet: " + str(self.score2)
                     font = pygame.font.Font('OpenSans-Regular.ttf', 25)
-                    self.drawText(text, font, (255, 255, 255), (math.floor((self.windowWidth / 2)), math.floor((self.windowHeight / 1.6))))
-                if self.difficulty.name == "easy":
+                    self.drawText(text, font, (255, 255, 255), (math.floor((self.windowWidth / 2)), math.floor((self.windowHeight / 1.7))))
+                    #Katsotaan kumpi pelaaja voitti
+                    if self.score1 > self.score2:
+                        text = "Pelaaja 1 voitti pelin!"
+                        font = pygame.font.Font('OpenSans-Regular.ttf', 40)
+                        self.drawText(text, font, (255, 255, 255), (math.floor((self.windowWidth / 2)), math.floor((self.windowHeight / 2.4))))
+                    elif self.score2 > self.score1:
+                        text = "Pelaaja 2 voitti pelin!"
+                        font = pygame.font.Font('OpenSans-Regular.ttf', 40)
+                        self.drawText(text, font, (255, 255, 255), (math.floor((self.windowWidth / 2)), math.floor((self.windowHeight / 2.4))))
+                #Nappien arvojen määrittäminen eri vaikeustasoille
+                if self.difficulty.name == "Easy" and self.gamemode.name == "Solo":
+                    fontsize = 30
+                    x = 60
+                    y = 220
+                    width = 280
+                    height = 75
                     restart = "easy"
-                elif self.difficulty.name == "normal":
+                elif self.difficulty.name == "Easy" and self.gamemode.name == "Duel":
+                    fontsize = 25
+                    x = 60
+                    y = 250
+                    width = 280
+                    height = 50
+                    restart = "easy_duel"
+                elif self.difficulty.name == "Normal" and self.gamemode.name == "Solo":
+                    fontsize = 40
+                    x = 100
+                    y = 400
+                    width = 400
+                    height = 75
                     restart = "normal"
-                elif self.difficulty.name == "hard":
+                elif self.difficulty.name == "Normal" and self.gamemode.name == "Duel":
+                    fontsize = 40
+                    x = 100
+                    y = 400
+                    width = 400
+                    height = 75
+                    restart = "normal_duel"
+                elif self.difficulty.name == "Hard" and self.gamemode.name == "Solo":
+                    fontsize = 40
+                    x = 150
+                    y = 400
+                    width = 400
+                    height = 75
                     restart = "hard"
+                elif self.difficulty.name == "Hard" and self.gamemode.name == "Duel":
+                    fontsize = 40
+                    x = 150
+                    y = 400
+                    width = 400
+                    height = 75
+                    restart = "hard_duel"
+                #Napin luonti
+                menu = Menu()
+                menu.button("PELAA UUDELLEEN", x, y, width, height, (150, 150, 150), (200, 200, 200), restart, fontsize)
+                menu.button("POISTU PELISTÄ", x, y+90, width, height, (185, 150, 150), (255, 150, 150), "quit", fontsize)
 
 
             # Metodia update() kutsutaan, jotta näyttö päivittyy...
