6f346a972c3f7007b0dc9d10cd30fe557fa6ebdb
Juhana Kuparinen <jmkupa@utu.fi>
2020-04-25 19:54:56 +0300

    Korona.

--- game_modules/Obstacle.py
+++ game_modules/Obstacle.py
@@ -7,14 +7,28 @@ class Obstacle:
         self.height = height
         self.width = width
 
+        self.stone = pygame.image.load('pictures/kivi.png')
+        self.bush = pygame.image.load('pictures/pensas.png')
+        self.corona = pygame.image.load('pictures/korona.png')
+
+        self.stone = pygame.transform.scale(self.stone, (20, 20))
+        self.bush = pygame.transform.scale(self.bush, (20, 20))
+        self.corona = pygame.transform.scale(self.corona, (20, 20))
+
     def newObstacle(self, gridSize):
+        self.randomObstacle = random.randint(0, 101)
         self.x = random.randint(0, self.width-1)
         self.y = random.randint(4, self.height-1)
         self.location = (self.x, self.y)
         self.obstacle = pygame.Rect(self.x * gridSize, self.y * gridSize, gridSize, gridSize)
 
     def drawObstacle(self):
-        pygame.draw.rect(pygame.display.get_surface(), (200, 0, 200), self.obstacle, 0)
+        if self.randomObstacle > 98:
+            pygame.display.get_surface().blit(self.corona, self.obstacle)
+        elif self.randomObstacle < 50:
+            pygame.display.get_surface().blit(self.bush, self.obstacle)
+        else:
+            pygame.display.get_surface().blit(self.stone, self.obstacle)
 
     def obstacleLocation(self):
         return self.location
